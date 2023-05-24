ab093643a710b8ac2867d859e40c2ecc2659f947
DennisKemboi <cherogonydennis9@gmail.com>
2023-05-24 18:56:07 +0300

    Revised

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -2,15 +2,27 @@ require 'pry'
 
 class String
 
+  attr_accessor(:name)
+
+  def initialize(name)
+    @@name = name
+  end
+
+
   def sentence?
+      # QUESTION - Mbona self.sentence haileti true nikipass @@name
+      # ANSWER - As we know self in refer to the the instance on which it was called on. Apa instance ni s1 - na s1 is not the same as @@name enye tumepitisha i.e s1 infact is a Ruby object with Object_ID
+      # So to overcome io, lazima tu assign s1 a value as in the calls uko chini
+    # @@name.end_with?(".") - ii niki uncomment, itareturn true na iishie apo iache next line of code enye natest
     self.end_with?(".")
   end
 
   def question?
+    @@name
     self.end_with?("?")
   end
 
-  def exclamation?
+  def exclamation?  
     self.end_with?("!")
   end
 
@@ -34,3 +46,14 @@ class String
   end
 
 end
+
+s1 = String.new("ii ni value tunapitisha tu.")
+s1 = "This is awesome."
+puts s1.sentence?
+
+s2 = String.new("But the fire is so delightful?")
+puts s2.question?
+
+# {Nataka io variable icha-njiwe ikiwa stored uko juu -- name
+# {So, ii haiwezekani kwa sababu -- self inarefer to the object/instance on which it was called i.e The class from which it was generated.}
+# Well I was wrong in the above 2 lines - Naweza tumia initialize na attr_accessor sp that statement inakuwa passed during instance definition.
